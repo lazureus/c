97b4a58a1a4433c8aa1389cafed29fa972442aa8
lazureus <lazureus86@gmail.com>
2013-12-12 15:20:39 +0100

    STRICT_ALIASING: add first test for the strict aliasing

---
+++ ___STRICT_ALIASING___/___TEST1___/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+uint32_t swap_words(uint32_t arg)
+{
+    uint16_t* const sp = (uint16_t*) &arg ;
+    uint16_t hi = sp[0] ;
+    uint16_t lo = sp[1] ;
+
+    sp[1] = hi ;
+    sp[0] = lo ;
+
+    return (arg) ;
+}
+
+int main(void)
+{
+    uint32_t a = 0x5A ;
+
+    printf("Hello world\n") ;
+
+    printf("Unswapped uint32_t word [%x]\n",a) ;
+
+    printf("Swapped uint32_t word [%x]\n",swap_words(a)) ;
+
+    return 0;
+}
